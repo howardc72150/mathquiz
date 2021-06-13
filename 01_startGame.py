baacd09b4c5e3907add2d0d2f1457209d4e8188a
howardc72150 <52299899+howardc72150@users.noreply.github.com>
2021-06-14 09:59:37 +1200

    updated

--- 01_startGame.py
+++ 01_startGame.py
@@ -9,7 +9,8 @@ class Start:
         self.headingLabel           = Label(self.labelBackgroundFrame, height=3, width=42, text="Math Quiz", font=("Verdana 15 bold"), 
                                             fg="#747475", justify=CENTER, bg="white")
         self.underLabelFrame        = Frame(width=600, height=7, bg="#e3e3e3")
-        self.startGameButton        = Button(text="PLAY", font=("Verdana 15 bold"), fg="#747475", border=0)
+        self.startGameButton        = Button(text="PLAY", font=("Verdana 15 bold"), fg="#747475", border=0,
+                                             command=self.toGame(self))
 
         self.headingLabel.grid()
         self.headingFrame.grid(row=0)
@@ -18,9 +19,18 @@ class Start:
         self.labelBackgroundFrame.grid(row=3)
         self.underLabelFrame.grid(row=4)
         self.startGameButton.grid(row=6, pady=50)
+
+    def toGame(self):
+        Game(self)
+
+
+class Game:
+    def __init__(self):
+        self.frame = Frame(width=200, height=200)
+        self.frame.grid()
         
-if __name__ == "__main__":
-    root = Tk()
-    root.title("Math Quiz")
-    var = Start(root)
-    root.mainloop()  
+        
+gui = Tk()
+gui.title("Math Quiz")
+var = Start(gui)
+gui.mainloop()  
