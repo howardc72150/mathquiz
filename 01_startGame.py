d8789a29254c822fa8b7a7c640d02db8c293a54d
howardc72150 <52299899+howardc72150@users.noreply.github.com>
2021-07-28 15:04:58 +1200

    updated

--- 01_startGame.py
+++ 01_startGame.py
@@ -1,8 +1,10 @@
+import random
 from tkinter import *
 from functools import partial
 
 class Start:
     def __init__(self, root):
+
         self.frame = Frame()
         self.frame.grid()
         self.headingFrame               = Frame(self.frame, width=500, height=18, bg="#CCE5FF")
@@ -28,6 +30,9 @@ class Start:
         
 class Game:
     def __init__(self):
+            # create random math equation
+        operators = ['+', '-', '*', '/']
+        equation = "{} {} {}".format(random.randint(0,12), random.choice(operators), random.randint(0,12))
         self.frame = Frame(width=500, height=600)
         self.frame.grid()
 
@@ -37,14 +42,14 @@ class Game:
                                                 fg="#6C6C6C", bg="#CCE5FF", width=45, height=2, justify=CENTER)
         self.info_frame                 = Frame(self.frame, width=500, height=100)
         self.question_info_frame        = Frame(self.frame, width=500, height=80, bg="white")
-        self.question_label             = Label(self.question_info_frame, bg="white", width=45, height=5, text="What is 5+4?", font=("Helvetica 15"), justify=CENTER)
+        self.question_label             = Label(self.question_info_frame, bg="white", width=45, height=5, text=equation, font=("Helvetica 15"), justify=CENTER)
         self.under_question_info_frame  = Frame(self.frame, width=500, height=5, bg="#E8E8E8")
         self.blank_frame                = Frame(self.frame, width=500, height=40)
         self.user_input                 = Entry(width=21, font=("Helvetica 15"))
         self.second_blank_frame         = Frame(width=500, height=40)
         self.help_button                = Button(text="Help", font=("Helvetica 12"), width=15, command=lambda: self.toHelp())
         self.stats_button               = Button(text="Statistics", font=("Helvetica 12"), width=15)
-
+    
         self.heading_label.grid(row=1)
         self.heading_frame.grid(row=1)
         self.under_heading_frame.grid(row=2)
@@ -57,12 +62,19 @@ class Game:
         self.second_blank_frame.grid(row=8)
         self.help_button.grid(row=9)
         self.stats_button.grid(row=10)
-    
+
+    def checkAnswer(self, equation):
+        user_input = self.user_input.get()
+        print("{} = {}").format(equation, eval(equation)))
+
+
     def toHelp(self):   
         self.frame.destroy()
         self.user_input.destroy()
         self.help_button.destroy()
+        self.blank_frame.destroy()
         self.stats_button.destroy()
+        self.second_blank_frame.destroy()
         Help()
 
 class Help:
@@ -73,18 +85,28 @@ class Help:
         self.heading_frame              = Frame(self.frame, width=500, height=20, bg="#CCE5FF")
         self.under_heading_frame        = Frame(self.frame, width=500, height=6, bg="#BDDEFF")
         self.blank_frame                = Frame(self.frame, height=50)
-        self.heading_label              = Label(self.frame, text="Help and Instructions", font=("Helvetica 15 bold"), fg="#6C6C6C", padx=50, justify=LEFT)
+        self.heading_label              = Label(self.frame, text="Help and Instructions", font=("Helvetica 15 bold"), fg="#6C6C6C", justify=CENTER)
         self.second_blank_frame         = Frame(self.frame, height=50)
-        self.instructions_text          = Frame(self.frame, text="Begin the quiz by clicking the Play button." \
-                                                                 "Type your answer in the entry form below" \
-                                                                 "the question and click the button to submit it.",  font=("Helvetica 15 bold"))
+        self.instructions_text          = Label(self.frame, text="Begin the quiz by clicking the Play button.\n"
+                                                                 "Type your answer in the entry form below\n"
+                                                                 "the question and click the button to submit it.",  font=("Helvetica 12"), justify=CENTER)
+        self.third_blank_frame          = Frame(self.frame, width=500, height=50)
+        self.dismiss_button             = Button(self.frame, text="Dismiss", font=("Helvetica 12"), width=15, command=lambda: self.dismissHelp())
+        self.fourth_blank_frame         = Frame(self.frame, width=500, height=35)
 
         self.heading_frame.grid()
         self.under_heading_frame.grid()        
         self.blank_frame.grid()
         self.heading_label.grid()
         self.second_blank_frame.grid() 
-        self.instructions_text.grid()    
+        self.instructions_text.grid()
+        self.third_blank_frame.grid()
+        self.dismiss_button.grid()
+        self.fourth_blank_frame.grid()
+    
+    def dismissHelp(self):
+        self.frame.destroy()
+        Game()
 
 
 gui = Tk()
